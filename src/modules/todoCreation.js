bce250fba91850908f3ce92d47782e74311c3a1f
Dawid <baluszynskidawid@gmail.com>
2021-04-06 23:07:22 +0200

    refactor: change start date format in todo factory function

--- src/modules/todoCreation.js
+++ src/modules/todoCreation.js
@@ -1,15 +1,15 @@
 import { format } from 'date-fns';
 
-const todoItem = ({ title, description, priority, project, endTask = '' }) => {
-  const getTimeToday = () => format(new Date(), 'dd/MM/yyyy');
+const todoItem = ({ title, description, priority, project, endDate = '' }) => {
+  const getTimeToday = () => format(new Date(), 'yyyy-MM-dd');
 
   const init = {
     title,
     description,
     priority,
     project,
-    startTask: getTimeToday(),
-    endTask,
+    startDate: getTimeToday(),
+    endDate,
     completed: false,
   };
 
